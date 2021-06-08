5110292f665bd1a3421e1aaf0c00b7a5c0ab58bd
ronald <ronaldjunior2@hotmail.com>
2021-06-08 15:39:34 -0300

    preparando review

--- src/components/HabitsList.js
+++
@@ -1,6 +0,0 @@
-import Habit from './Habit'
-export default function HabitsList({habits,setHabits}){
-    return(
-       habits.map(habit => <Habit habit={habit} habits={habits} setHabits={setHabits} />) 
-    )
-}
